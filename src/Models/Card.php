df51d8507912a5aee593a206d3b07838638587ce
Dimitar Dishev <dimitar.dishev@ampeco.global>
2022-08-05 15:41:02 +0300

    upd: bin details brand and bin_number are optional

--- src/Models/Card.php
+++ src/Models/Card.php
@@ -4,18 +4,18 @@ namespace Ampeco\OmnipayRapyd\Models;
 
 class Card
 {
-    public string $cardType;
+    public ?string $cardType;
     public string $token;
-    public string $firstSix;
+    public ?string $firstSix;
     public string $lastFour;
     public string $month;
     public string $year;
 
     public function __construct(array $data)
     {
-        $this->cardType = $data['bin_details']['brand'];
+        $this->cardType = $data['bin_details']['brand'] ?? null;
         $this->token = $data['id'];
-        $this->firstSix = $data['bin_details']['bin_number'];
+        $this->firstSix = $data['bin_details']['bin_number'] ?? null;
         $this->month = intval($data['expiration_month']);
         $this->year = 2000 + intval($data['expiration_year']);
         $this->lastFour = $data['last4'];
